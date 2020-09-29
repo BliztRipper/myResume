d7613cf9850806955d39979da2a95a9b297ed4e8
Arnon Tawong <arnontawong@Arnons-MacBook-Pro.local>
2020-09-29 16:45:44 +0700

    Updates

---
+++ precache-manifest.ed1e68fb93f92e690d9cdfa2e4a786eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb7c0bc81846a28677d8ff8e8d9ab95",
+    "url": "/myResume/index.html"
+  },
+  {
+    "revision": "522e0025477103ba7a4b",
+    "url": "/myResume/static/css/2.ebe13ea2.chunk.css"
+  },
+  {
+    "revision": "888ab73bd1f3c2a005eb",
+    "url": "/myResume/static/css/main.d175a009.chunk.css"
+  },
+  {
+    "revision": "522e0025477103ba7a4b",
+    "url": "/myResume/static/js/2.005e45be.chunk.js"
+  },
+  {
+    "revision": "888ab73bd1f3c2a005eb",
+    "url": "/myResume/static/js/main.6677590e.chunk.js"
+  },
+  {
+    "revision": "7c6903d8f0ca3abfe94b",
+    "url": "/myResume/static/js/runtime~main.4fcd64c1.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/myResume/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/myResume/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/myResume/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/myResume/static/media/slick.f97e3bbf.svg"
+  }
+]);
